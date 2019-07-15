643a4d38b74fd978de08ad4afa0f2d9a7aee5a25
sukhbir-singh <sukhbir947@gmail.com>
2019-07-16 02:08:55 +0530

    small changes

--- app/models/friend_user.rb
+++ app/models/friend_user.rb
@@ -3,7 +3,14 @@ class FriendUser < ApplicationRecord
   belongs_to :system
 
   def self.get_users(system_id)
-    FriendUser.where(system_id: system_id)
+    users = FriendUser.where(system_id: system_id)
+    new_users = []
+    users.each do |user|
+      new_users << user.as_json.merge({
+        email: user.amahi_user.email
+      })
+    end
+    return new_users
   end
 
   def self.create(args)
